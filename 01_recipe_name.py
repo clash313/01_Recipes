3a0e989f2d2cd3d94dbe6a9249ffc34c31a0c183
clash313 <60630460+clash313@users.noreply.github.com>
2020-02-17 13:05:45 +1300

    working name checker

--- 01_recipe_name.py
+++ 01_recipe_name.py
@@ -1,13 +1,25 @@
 # Get's recipe name and checks it is not blank
 
+
 # Not Blank Function goes here
 def not_blank(question):
+    error = "Your recipe name has numbers in it."
 
     valid = False
     while not valid:
         response = input(question)
+        has_errors = ""
+
+        # look at each character in string and if it's a number, complain
+        for letter in response:
+            if letter.isdigit():
+                has_errors = "yes"
+                break
 
         if response == "":
+            continue
+        elif has_errors != "":
+            print(error)
             continue
         else:
             return response
