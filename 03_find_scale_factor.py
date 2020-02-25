621397ac114a18d36a23de92ad7fb3d984a9f995
clash313 <60630460+clash313@users.noreply.github.com>
2020-02-26 10:16:21 +1300

    Update 03_find_scale_factor.py
    checks that number inputs are more than zero

--- 03_find_scale_factor.py
+++ 03_find_scale_factor.py
@@ -11,11 +11,18 @@
 def num_check(question):
 
     error = "Please enter a number that is more than zero"
+
     valid = False
     while not valid:
         try:
             response = float(input(question))
-            return response
+
+            if response <= 0:
+                print(error)
+            else:
+
+
+             return response
 
         except ValueError:
             print(error)
