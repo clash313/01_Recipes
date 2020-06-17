5ba8e6a74c1655158d3c64d0e1705fea8f0e295f
clash313 <60630460+clash313@users.noreply.github.com>
2020-06-18 09:54:14 +1200

    Update 09_ingredient_splitter.py

--- 09_ingredient_splitter.py
+++ 09_ingredient_splitter.py
@@ -3,4 +3,33 @@ import re
 # ingredient has mixed fraction followed by unit and imgredient
 recipe_line = "1 1/2 ml flour"  # Change to input statement in due course
 
-mixed_regex= ""
+mixed_regex= "\d{1,3}\s\d{1,3}\/\d{1,3}"
+
+if re.match(mixed_regex, recipe_line):
+    print("true")
+    # Get mixed number by matching the regex
+    pre_mixed_num = re.match(mixed_regex, recipe_line)
+    mixed_num = pre_mixed_num.group()
+
+    # Replace space with a + sign...
+    amount = mixed_num.replace(" ", "+")
+    # Change the string into a decimal
+    amount = eval(amount)
+    print(amount)
+
+    # Get unit and ingredients...
+    compile_regex = re.compile(mixed_regex)
+    print(compile_regex)
+    unit_ingredient = re.split(compile_regex, recipe_line)
+    unit_ingredient = (unit_ingredient[1]).strip()  # remove extra white space from
+    print(unit_ingredient)
+
+get_unit = unit_ingredient.split(" ", 1)    # splits text at first space
+print (get_unit)
+
+unit = get_unit[0]
+ingredient = get_unit[1]
+
+print(unit)
+print(ingredient)
+
