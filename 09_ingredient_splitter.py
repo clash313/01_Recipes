c46b1f23b9d94180ba4f7636a4df56674d5f993b
clash313 <60630460+clash313@users.noreply.github.com>
2020-06-22 13:10:33 +1200

    Update 09_ingredient_splitter.py

--- 09_ingredient_splitter.py
+++ 09_ingredient_splitter.py
@@ -1,35 +1,53 @@
 import re
 
 # ingredient has mixed fraction followed by unit and imgredient
-recipe_line = "1 1/2 ml flour"  # Change to input statement in due course
+
+full_recipe =[
+    "1 1/2 ml flour",
+    "3/4 cup milk",
+    "1 cup flour",
+    "2 tablespoons white sugar",
+    "1.5 tsp baking powder",
+    "pinch of cinammon"
+]
 
 mixed_regex= "\d{1,3}\s\d{1,3}\/\d{1,3}"
 
-if re.match(mixed_regex, recipe_line):
-    print("true")
-    # Get mixed number by matching the regex
-    pre_mixed_num = re.match(mixed_regex, recipe_line)
-    mixed_num = pre_mixed_num.group()
+for recipe_line in full_recipe:
+    recipe_line = recipe_line.strip()
 
-    # Replace space with a + sign...
-    amount = mixed_num.replace(" ", "+")
-    # Change the string into a decimal
-    amount = eval(amount)
-    print(amount)
+    # Get amount...
+    if re.match(mixed_regex, recipe_line):
+        # Get mixed number by matching the regex
+        pre_mixed_num = re.match(mixed_regex, recipe_line)
+        mixed_num = pre_mixed_num.group()
 
-    # Get unit and ingredients...
-    compile_regex = re.compile(mixed_regex)
-    print(compile_regex)
-    unit_ingredient = re.split(compile_regex, recipe_line)
-    unit_ingredient = (unit_ingredient[1]).strip()  # remove extra white space from
-    print(unit_ingredient)
+        # Replace space with a + sign...
+        amount = mixed_num.replace(" ", "+")
+        # Change the string into a decimal
+        amount = eval(amount)
 
-get_unit = unit_ingredient.split(" ", 1)    # splits text at first space
-print (get_unit)
+        # Get unit and ingredients...
+        compile_regex = re.compile(mixed_regex)
+        print(compile_regex)
+        unit_ingredient = re.split(compile_regex, recipe_line)
+        unit_ingredient = (unit_ingredient[1]).strip()  # remove extra white space
 
-unit = get_unit[0]
-ingredient = get_unit[1]
+    else:
+        get_amount = recipe_line.split(" ", 1)  # splits line at first space
 
-print(unit)
-print(ingredient)
+        try:
+            amount = eval(get_amount[0])    # convert amount to float if possible
+        except NameError:
+            amount = get_amount[0]
+            convert = "no"
 
+        unit_ingredient = get_amount[1]
+
+
+    # Get unit and ingredient...
+    get_unit = unit_ingredient.split(" ", 1)    # splits text at first space
+    unit = get_unit[0]
+    ingredient = get_unit[1]
+
+    print("{} {} {}".format(amount, unit, ingredient))
