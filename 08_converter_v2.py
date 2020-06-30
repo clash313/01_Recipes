16ae59e5e1534727fac7a7faf3870e57a2cad4b7
clash313 <60630460+clash313@users.noreply.github.com>
2020-06-30 12:14:11 +1200

    Update 08_converter_v2.py

--- 08_converter_v2.py
+++ 08_converter_v2.py
@@ -6,14 +6,14 @@ import csv
 def general_converter(how_much, lookup, dictionary, conversion_factor):
 
     if lookup in dictionary:
-        mult_by = dictionary.get(unit)
-        how_much = how_much * mult_by * conversion_factor
-
+        mult_by = dictionary.get(lookup)
+        how_much = how_much * float(mult_by) / conversion_factor
+        converted = "yes"
 
     else:
         converted = "no"
 
-    return how_much
+    return [how_much, converted]
 
 def unit_checker():
 
@@ -22,7 +22,7 @@ def unit_checker():
     # Abbreviation lists
     teaspoon = ["tsp", "teaspoon", "t"]
     tablespoon = ["tbs", "tablespoon", "T", "tbsp"]
-    ounce = ["oz", "ounce", "fl oz"]
+    ounce = ["oz", "ounce", "fl oz", "ounces"]
     cup = ["c", "cup", "cups"]
     pint = ["p", "pt", "fl pt"]
     quart = ["q", "qt", "fl qt"]
@@ -33,7 +33,6 @@ def unit_checker():
     if unit_tocheck == "":
     # print("you choose {}".format(unit_tocheck))
         return unit_tocheck
-
     elif unit_tocheck == "T" or unit_tocheck.lower() in tablespoon:
         return "tbs"
     elif unit_tocheck.lower() in teaspoon:
@@ -71,3 +70,51 @@ unit_central = {
 # *** Generate food dictionary *****
 # open file
 groceries = open('01_ingredients_ml_to_g.csv')
+
+
+# Read data into a list
+csv_groceries = csv.reader(groceries)
+
+# Create a dictionary to hold the data
+food_dictionary = {}
+
+# Add the data from the list into the dictionary
+# (first item in row is key, next is definition)
+
+for row in csv.groceries:
+    food_dictionary[row[0]] = row[1]
+
+    print(food_dictionary)
+
+# **** Get items etc *****
+
+keep_going = ""
+while keep_going == "":
+    amount = eval(input("How much?"))
+    amount = float(amount)
+
+    # Get unit and change  it to match dictionary
+    unit = unit_checker()
+    ingredient = input("Ingredient: ")
+
+    # Convert to mls if possible
+    amount = general_converter(amount, unit, unit_central, 1)
+    print(amount)
+
+    # If we converted to mls, try and convert to grams
+    if amount[1] == "yes":
+        amount_2 = general_converter(amount[0], ingredient, food_dictionary, 250)
+
+        # if the ingredient is in the list, convert it
+        if amount_2[1] == "yes":
+            print(amount_2)
+
+        # if the ingredient is not in the list, leave the unit as ml
+        else: print("unchanged")
+
+    # If the unit is not mls, leave the line unchanged
+    else:
+        print("unchanged")
+
+    # keep_going = input("<enter> or q")
+
